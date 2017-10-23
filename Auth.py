46a14872f825f40d193ef871dedac677d03eeed3
NikDem <nikdem000@mail.ru>
2017-10-23 19:26:32 +0300

    update

--- Auth.py
+++ Auth.py
@@ -1,10 +1,11 @@
-from os import getcwd
+from os import getcwd,mkdir
 import time
 
 def Auth_user():
     try:
-        f = open('file.txt', 'r+')
+        f = open(getcwd()+'/Info/file.txt', 'r+')
     except FileNotFoundError:
+        mkdir(getcwd()+'/Info')
         f = open(getcwd()+'/Info/file.txt', 'w+')
         login = 'Login:' + input('Информация о пользователе не найдена\n'
                               'Авторизация Нового Пользователя\n'
