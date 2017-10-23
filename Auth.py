33b7f58143b660c105d9f5fc3bcc7d19e6f37a42
NikDem <nikdem000@mail.ru>
2017-10-23 23:34:40 +0300

    Update

--- Auth.py
+++ Auth.py
@@ -9,16 +9,23 @@ def Auth_user():
     except FileNotFoundError:
         mkdir(getcwd()+'/Info')
         f = open(getcwd()+'/Info/file.txt', 'w+')
-        login = 'Login:' + input('Информация о пользователе не найдена\n'
-                                 'Авторизация Нового Пользователя\n'
-                                 'Login:')+'\n'
-        password = 'Password:' + input('Password:') + '\n'
+        login = input('Информация о пользователе не найдена\n'
+                      'Авторизация Нового Пользователя\n'
+                      'Login:')+'\n'
+        password = input('Password:')
         f.write(login)
         f.write(password)
     f.seek(0)
+    path = getcwd()+'/Info/Users.txt'
     disk = YaDisk(input('Ya_Login:'), input('Ya_Password:'))
-    disk.download('Logs/Users.txt',getcwd()+'/Info/Users.txt')
-    remove(getcwd()+'/Info/Users.txt')
+    disk.download('Logs/Users.txt',path)
+    g = open(path,'r')
+    s = g.read()
+    g.close()
+    if s!=(login+'\n'+password):
+        print('Такого пользователя в базе данных нет, exit')
+        exit()
+    #remove(path)
     print('\nВыполняется вход...\n')
     time.sleep(1)
     user_login =  f.readline().replace('Login:','')
